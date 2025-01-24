3b804faa5643f00310576d67db3031e4c3dd67e0
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-24 10:18:09 +0100

    [KBW-8][ADD] RoundInfo component

---
+++ src/components/header/RoundInfo.tsx
@@ -0,0 +1,14 @@
+interface RoundInfoContainerInterface {
+
+}
+
+const RoundInfoContainer: React.FC<RoundInfoContainerInterface> = ({ }) => {
+
+    return (
+        <div className="fixed top-0 left-1/2 transform -translate-x-1/2 flex flex-col justify-start items-center w-1/6 h-3/12 rounded-md border-2 border-white-300">
+
+        </div>
+    );
+};
+
+export default RoundInfoContainer;
