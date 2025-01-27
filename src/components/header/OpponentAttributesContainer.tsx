0201002139937757f14f6e94db1ebc837e21fb55
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-27 12:35:51 +0100

    [KBW-26][ADD] Opponent life bar component

--- src/components/header/OpponentAttributesContainer.tsx
+++ src/components/header/OpponentAttributesContainer.tsx
@@ -1,4 +1,4 @@
-import rightLifeBar from '/images/life_bar_right.gif'
+import OpponentLifeBar from './OpponentLifeBar';
 
 interface OpponentAttributesContainerInterface {
     opponent: any
@@ -8,12 +8,10 @@ const OpponentAttributesContainer: React.FC<OpponentAttributesContainerInterface
 
     return (
         <div className="flex flex-col justify-items-start items-start h-[40%] w-[27%] mr-[2.5%] mt-[5.2%] rounded-md">
-            <div className="flex h-full w-full rounded-md items-center justify-center z-0">
-                <img src={rightLifeBar}
-                    alt="Life Bar"
-                    className="h-full w-full object-cover"
-                />
-            </div>
+
+            {/* Opponent Life Bar */}
+            <OpponentLifeBar />
+
             <div className="flex h-full w-[65%] ml-auto rounded-md items-center justify-center z-20">
                 {opponent.name}
             </div>
