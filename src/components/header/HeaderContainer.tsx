2f86fcdf70827ac9beac7e47fa3929f41d756ff5
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-27 08:25:14 +0100

    [DEVELOP][DELETE] Borders component

--- src/components/header/HeaderContainer.tsx
+++ src/components/header/HeaderContainer.tsx
@@ -7,7 +7,7 @@ interface HeaderInfoContainer {
 const HeaderContainer: React.FC<HeaderInfoContainer> = ({ }) => {
 
     return (
-        <div className="flex flex-row w-full h-4/12 justify-center items-center rounded-md border-1 border-white-300">
+        <div className="flex flex-row w-full h-4/12 justify-center items-center rounded-md">
             
             {/* <RoundContainer/> */}
             <RoundContainer/>
--- src/components/header/RoundContainer.tsx
+++ src/components/header/RoundContainer.tsx
@@ -5,7 +5,7 @@ interface RoundContainerInterface {
 const RoundContainer: React.FC<RoundContainerInterface> = ({ }) => {
 
     return (
-        <div className="flex flex-col h-full w-1/12 justify-center items-center rounded-md border-1 border-amber-300">
+        <div className="flex flex-col h-full w-1/12 justify-center items-center rounded-md">
             
         </div>
     );
