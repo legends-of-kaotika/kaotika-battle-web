4906b68d0569299463194c4f302adfab1a1807af
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-27 09:32:44 +0100

    [KBW-5][DELETE] Borders color

--- src/components/header/OpponentAttributesContainer.tsx
+++ src/components/header/OpponentAttributesContainer.tsx
@@ -5,7 +5,7 @@ interface OpponentAttributesContainerInterface {
 const OpponentAttributesContainer: React.FC<OpponentAttributesContainerInterface> = ({ }) => {
 
     return (
-        <div className="justify-items-start items-start flex h-[50%] w-[27%] mr-[2.5%] mt-[4%] rounded-md border-1 border-red-500">
+        <div className="justify-items-start items-start flex h-[50%] w-[27%] mr-[2.5%] mt-[4%] rounded-md border-1">
 
         </div>
     );
