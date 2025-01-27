a49342410fda986e5125bbfa44b6a46dcf001f55
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-01-27 10:29:43 +0100

    [KBW-10][ADD]Battle components

---
+++ src/components/battle/BattleContainer.tsx
@@ -0,0 +1,13 @@
+import PotionContainer from "../Potions/PotionContainer";
+import Battle from "./Battle";
+
+const BattleContainer = () => {
+    return (
+        <div className="w-[100%] h-[50%] flex -mt-[5%] justify-around">
+            <PotionContainer healing={true} antidote={true} enhancer={true}></PotionContainer>
+            <Battle/>
+        </div>
+    )
+}
+
+export default BattleContainer;
---
+++ src/components/battle/Oponent.tsx
@@ -0,0 +1,11 @@
+
+
+const Oponent = () => {
+    return(
+        <div className="w-[25%] h-[98%] border-2 border-orange-400 -ml-[10%]">
+
+        </div>
+    )
+}
+
+export default Oponent;
