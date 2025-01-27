dfee32b989800d5a1aabe80b85c60893b446b890
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-27 12:36:03 +0100

    ADD- the players fake data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
-import HeaderContainer from './components/header/HeaderContainer'
-import borderImage from '../public/images/header_border.png'
-import BattleContainer from './components/battle/BattleContainer'
+import './App.css';
+import HeaderContainer from './components/header/HeaderContainer';
+import borderImage from '../public/images/header_border.png';
+import BattleContainer from './components/battle/BattleContainer';
 import Hud from './components/footer/Hud';
+import { useState } from 'react';
+import { attackerData, defenderData } from './constants/playersData';
 
 function App() {
+  const [attacker, setAttacker] = useState(attackerData);
+  const [defender, setDefender] = useState(defenderData);
 
   return (
     <div className='w-screen h-screen bg-center bg-cover' style={{ backgroundImage: `url(${borderImage})` }}>
