14a31cee2392ab7cef0f3d8b11f41e4bdc38b4a3
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-27 12:59:04 +0100

    [DEVELOP][DELETE] Battle border

--- src/components/battle/Oponent.tsx
+++ src/components/battle/Oponent.tsx
@@ -2,7 +2,7 @@
 
 const Oponent = () => {
   return (
-    <div className="w-[25%] h-[98%] border-2 border-orange-400 -ml-[10%] mt-[3%]">
+    <div className="w-[25%] h-[98%] -ml-[10%] mt-[3%]">
     </div>
   )
 }
