3aaf42999b902e8f83b95a3c58c7aa58b567a577
llabaka <lander.labaka@ikasle.aeg.eus>
2025-01-27 12:42:20 +0100

    [KBW-26][ADD] Attacker life bar component

--- src/components/header/AttackerAttributesContainer.tsx
+++ src/components/header/AttackerAttributesContainer.tsx
@@ -1,4 +1,4 @@
-import leftRightBar from '/images/life_bar_left.gif'
+import AttackerLifeBar from "./AttackerLifeBar";
 
 interface AttackerAttributesInterface {
     player: any
@@ -8,12 +8,10 @@ const AttackerAttributesContainer: React.FC<AttackerAttributesInterface> = ({ pl
 
     return (
         <div className="flex flex-col justify-items-start items-start h-[40%] w-[27%] ml-[2.5%] mt-[5.2%] rounded-md">
-            <div className="flex h-full w-full rounded-md items-center justify-center z-0">
-                <img src={leftRightBar}
-                    alt="Life Bar"
-                    className="h-full w-full object-cover"
-                />
-            </div>
+
+            {/* Attacker Life Bar */}
+            <AttackerLifeBar />
+
             <div className="flex h-full w-[65%] rounded-md items-center justify-center z-20">
                 {player.name}
             </div>
