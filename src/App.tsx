122127e6d439cb25ce4c57e4f8af3f0c02818c49
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-01-28 08:08:07 +0100

    [KBW-23]clean footer componenets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import React, { useState, useEffect } from 'react';
 import HeaderContainer from './components/header/HeaderContainer'
 import borderImage from '../public/images/header_border.png'
 import Hud from './components/footer/Hud';
 import BattleContainer from './components/battle/BattleContainer'
 
+
 function App() {
+
+  const [actions, setActions] = useState<string[]>([]);
+
+ 
+  const addAction = (action: string) => {
+      setActions([...actions, action]);
+  };
+
+  useEffect(() => {
+    addAction('The Calborot attacks!');
+  }, []);
+
 
   return (
     <div className='w-screen h-screen bg-center bg-cover' style={{ backgroundImage: `url(${borderImage})` }}>
@@ -15,7 +29,7 @@ function App() {
       <BattleContainer/>
 
       {/* Footer Container */}
-      <Hud currentPhase='attack'/>
+      <Hud/>
 
 
     </div>
--- src/components/footer/Hud.tsx
+++ src/components/footer/Hud.tsx
@@ -1,17 +1,12 @@
-import History from "./History";
-import TurnPhotos from "./TurnPhotos";
-import Phase from "./Phase";
 
 interface Hud {
-    currentPhase: 'attack' | 'potions';
+
 }
 
-const Hud: React.FC<Hud> = ({currentPhase}) => {
+const Hud: React.FC<Hud> = ({}) => {
     return (
         <div className="w-full h-[25%] flex justify-between fixed bottom-0">
-            <Phase phase={currentPhase}/>
-            <TurnPhotos />
-            <History/>
+         
         </div>
     )
 }
