0317d31d5152b13aaa43a56c5c9d86faf889a700
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-28 12:48:15 +0100

    [KBW][ADD] - the life reduction to the life bar

--- src/components/header/OpponentAttributesContainer.tsx
+++ src/components/header/OpponentAttributesContainer.tsx
@@ -7,13 +7,13 @@ interface OpponentAttributesContainerInterface {
 const OpponentAttributesContainer: React.FC<OpponentAttributesContainerInterface> = ({ player }) => {
 
   return (
-    <div className="flex flex-col justify-items-start items-start h-[40%] w-[27%] mr-[2.5%] mt-[5.2%] rounded-md">
+    <div className="flex flex-col justify-items-start items-start h-[40%] w-[27%] mr-[2.5%] mt-[5.6%] rounded-md">
 
       {/* Opponent Life Bar */}
       <OpponentLifeBar maxHitpoints={player.maxHitpoints} hitpoints={player.hitpoints}/>
 
       <div className="flex h-full w-[65%] ml-auto rounded-md items-center justify-center text-2xl z-20">
-        {player.name}
+        {player.nickname}
       </div>
     </div>
   );
--- src/components/header/OpponentLifeBar.tsx
+++ src/components/header/OpponentLifeBar.tsx
@@ -1,20 +1,27 @@
-import rightLifeBar from '/images/life_bar_right.gif'
+import { useEffect, useState } from 'react';
+import rightLifeBar from '/images/life_bar_right.gif';
+import missingRightLifeBar from '/images/missing_life_bar_right.png';
 
 interface OpponentLifeBarInterface {
-  maxHitpoints: number,
-  hitpoints: number,
+  maxHitpoints: number;
+  hitpoints: number;
 }
 
 const OpponentLifeBar: React.FC<OpponentLifeBarInterface> = ({ maxHitpoints, hitpoints }) => {
+  const [life, setLife] = useState(hitpoints / maxHitpoints * 100);
+  const [missingLife, setMissingLife] = useState((maxHitpoints - hitpoints) / maxHitpoints * 100);
+
+  useEffect(() => {
+    setLife(hitpoints / maxHitpoints * 100);
+    setMissingLife((maxHitpoints - hitpoints) / maxHitpoints * 100);
+  }, [hitpoints]);
 
   return (
-    <div className="flex h-full w-full rounded-md items-center justify-center z-0">
-      <img src={rightLifeBar}
-        alt="Life Bar"
-        className="h-full w-full object-cover"
-      />
+    <div className="flex inline-row h-[60%] w-full">
+      <div className={`bg-left bg-cover`} style={{ backgroundImage: `url(${rightLifeBar})`, width: `${life}%` }} />
+      <div className={`bg-left bg-cover`} style={{ backgroundImage: `url(${missingRightLifeBar})`, width: `${missingLife}%` }} />
     </div>
-  );
-};
+  )
+}
 
 export default OpponentLifeBar;
