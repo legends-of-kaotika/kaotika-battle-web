6409079a12b2c681381c22f5ca108e68c1175d58
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-01-29 11:59:22 +0100

    [KBW-46][ADD]Zustand for global state

---
+++ src/Interfaces/AppState.ts
@@ -0,0 +1,8 @@
+import { Socket } from "socket.io-client";
+import { Player } from "./Player";
+
+export interface AppState{
+    players : Player[];
+    addPlayer: (player: Player) => void;
+    socket: Socket
+}
