e9af8de1c282843a286385b37f8d3bf8c89ecb82
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-02-03 11:56:18 +0100

    [KBW-73]remove import

--- src/components/header/LeftPlayerAttributesContainer.tsx
+++ src/components/header/LeftPlayerAttributesContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import useStore from "../../store/store";
 import LeftPlayerLifeBar from "./LeftPlayerLifeBar";
 import { attackerData } from "../../constants/playersData";
