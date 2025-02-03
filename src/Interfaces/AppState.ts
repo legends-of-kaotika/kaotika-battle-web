cc825a7b0bf4372ef56101e30bbd4985f34f5ee8
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-03 13:27:50 +0100

    [DEVELOP][UPDATE] Spaces in appstate

--- src/Interfaces/AppState.ts
+++ src/Interfaces/AppState.ts
@@ -4,18 +4,18 @@ import { Player } from './Player';
 import { PlayersRole } from './PlayerRole';
 
 export interface AppState {
-    players: PlayersRole;
-    addKaotika: (kaotika: Player) => void;
-    addDravocar: (dravocar: Player) => void;
-    socket: Socket;
-    round: number;
-    addRound: (by: number) => void;
-    attack: (hit_points: number) => void;
-    attacker: Player | null;
-    defender: Player | null;
-    setAttacker: (attacker: Player | null) => void;
-    setDefender: (defender: Player | null) => void;
-    setPlayers: (players: PlayersRole) => void;
-    timer: number;
-    setTimer: (timer: number) => void;
+  players: PlayersRole;
+  addKaotika: (kaotika: Player) => void;
+  addDravocar: (dravocar: Player) => void;
+  socket: Socket;
+  round: number;
+  addRound: (by: number) => void;
+  attack: (hit_points: number) => void;
+  attacker: Player | null;
+  defender: Player | null;
+  setAttacker: (attacker: Player | null) => void;
+  setDefender: (defender: Player | null) => void;
+  setPlayers: (players: PlayersRole) => void;
+  timer: number;
+  setTimer: (timer: number) => void;
 }
