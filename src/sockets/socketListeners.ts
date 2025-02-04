b45a48566aa1758bca84abc7bf99cd9e503a2ce5
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-04 11:32:56 +0100

    [KBW-76][ADD] SocketListeners file

---
+++ src/sockets/socketListeners.ts
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import { Player } from '../Interfaces/Player';
+import { PlayersRole } from '../Interfaces/PlayerRole';
+import getPlayerById from '../helpers/getPlayerById';
+import updatePlayerById from '../helpers/updatePlayerById';
+import useStore from '../store/store';
+import { webSendUserHandler } from './webSendUserHandler';
+
+export const useSocketListeners = () => {
+  const { players, socket, setPlayers, setDefender, timer, setAttacker } = useStore();
+  const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+  const [startBattle, setStartBattle] = useState<boolean>(false);
+  const [finishTurn, setFinishTurn] = useState<boolean>(false);
+
+  useEffect(() => {
+    socket.emit('web-sendSocketId');
+    socket.emit('web-sendUsers');
+  }, []);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    //Add kaotika and dravocar
+    webSendUserHandler();
+
+    socket.on('connectedUsers', (data : PlayersRole) => {
+      console.log(1);
+
+      setPlayers(data);
+    });
+    socket.on('gameStart', () => {
+      setStartBattle(true);
+    });
+
+    socket.on('web-setSelectedPlayer', (id: string) => {
+      setDefender(getPlayerById(players, id)!);
+    });
+
+    socket.on('updatePlayer', (id: string, attr: Partial<Player>, totalDamage: number) => {
+      console.log('daño: ' + totalDamage);
+      setPlayers(updatePlayerById(players, id, attr));
+      socket.emit('web-turnEnd');
+      setFinishTurn(true);
+    });
+
+    socket.on('assign-turn', (id: string) => {
+      setAttacker(getPlayerById(players, id)!);
+      setFinishTurn(false);
+    });
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('connectedUsers');
+      socket.off('web-sendUser');
+      socket.off('gameStart');
+      socket.off('web-setSelectedPlayer');
+      socket.off('updatePlayer');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('Connected to socket server: ' + isConnected);
+  }, [isConnected]);
+
+  useEffect(() => {
+    if (timer === 0) {
+      socket.emit('web-turnEnd');
+      setFinishTurn(true);
+    };
+  }, [socket, timer]);
+
+  return { startBattle, finishTurn };
+};
--- src/store/store.ts
+++ src/store/store.ts
@@ -29,7 +29,7 @@ const useStore = create<AppState>()((set) => ({
   setAttacker: (attacker: Player | null) => set(() => ({ attacker: attacker })),
   setDefender: (defender: Player | null) => set(() => ({ defender: defender })),
   setPlayers: (players: PlayersRole) => set(() => ({ players: players })),
-  timer: 0,
+  timer: -1,
   setTimer: (timer: number) => set(() => ({ timer: timer })),
 
 
