34fd817b585905b0c36ba70032c183f545c5fbff
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-02-04 08:09:02 +0100

    [KBW-75][FIX]Update player by Id

--- src/helpers/updatePlayerById.ts
+++ src/helpers/updatePlayerById.ts
@@ -2,9 +2,7 @@ import { Player } from '../Interfaces/Player';
 import { PlayersRole } from '../Interfaces/PlayerRole';
 
 export default function updatePlayerById(players: PlayersRole, id: string, changes: Partial<Player>) : PlayersRole{
-
-  const allPlayers = [...players.dravocar, ...players.kaotika];
-  allPlayers.map(player => player._id === id ? {...player, ...changes} : player);
+  const allPlayers = [...players.dravocar, ...players.kaotika].map(player => player._id === id ? {...player, ...changes} : player);
 
   return {
     dravocar: allPlayers.filter(player => player.isBetrayer === true),
--- src/store/store.ts
+++ src/store/store.ts
@@ -8,29 +8,29 @@ const SERVER_URL = 'https://kaotika-battle-server.onrender.com';
 
 const useStore = create<AppState>()((set) => ({
 
-  players: { dravocar: [], kaotika: [] },
-  addKaotika: (kaotika: Player) => set((state) => ({ players: { ...state.players, kaotika: [...state.players.kaotika, kaotika] } })),
-  addDravocar: (dravocar: Player) => set((state) => ({ players: { ...state.players, dravocar: [...state.players.dravocar, dravocar] } })),
-  socket: io(SERVER_URL),
-  round: 1,
-  addRound: (by: number) => set((state) => ({ round: state.round += by })),
-  attack: (hit_points: number) => set((state) => ({
-    defender: {
-      ...state.defender!,
-      attributes: {
-        ...state.defender!.attributes,
-        hit_points: state.defender!.attributes.hit_points - hit_points
-      }
-    }
-  })),
-  attacker: null,
-  defender: null,
+	players: { dravocar: [], kaotika: [] },
+	addKaotika: (kaotika: Player) => set((state) => ({ players: { ...state.players, kaotika: [...state.players.kaotika, kaotika] } })),
+	addDravocar: (dravocar: Player) => set((state) => ({ players: { ...state.players, dravocar: [...state.players.dravocar, dravocar] } })),
+	socket: io(SERVER_URL),
+	round: 1,
+	addRound: (by: number) => set((state) => ({ round: state.round += by })),
+	attack: (hit_points: number) => set((state) => ({
+		defender: {
+			...state.defender!,
+			attributes: {
+				...state.defender!.attributes,
+				hit_points: state.defender!.attributes.hit_points - hit_points
+			}
+		}
+	})),
+	attacker: null,
+	defender: null,
 
-  setAttacker: (attacker: Player | null) => set(() => ({ attacker: attacker })),
-  setDefender: (defender: Player | null) => set(() => ({ defender: defender })),
-  setPlayers: (players: PlayersRole) => set(() => ({ players: players })),
-  timer: 0,
-  setTimer: (timer: number) => set(() => ({ timer: timer })),
+	setAttacker: (attacker: Player | null) => set(() => ({ attacker: attacker })),
+	setDefender: (defender: Player | null) => set(() => ({ defender: defender })),
+	setPlayers: (players: PlayersRole) => set(() => ({ players: players })),
+	timer: 0,
+	setTimer: (timer: number) => set(() => ({ timer: timer })),
 
 
 }));
