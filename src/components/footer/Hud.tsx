a2847696ff8ba73f189b4d92a09d0c777e38874f
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-02-04 09:00:01 +0100

    [KBW-78]Fix hud component

--- src/components/footer/Hud.tsx
+++ src/components/footer/Hud.tsx
@@ -1,18 +1,10 @@
-import React, { useEffect } from 'react';
-import { Player } from '../../Interfaces/Player';
+import React from 'react';
 import useStore from '../../store/store';
 import PlayerAvatar from './PlayerAvatar';
 
 const Hud: React.FC = () => {
-
-  const { players} = useStore();
-  const [dravocar, setDravocar] = React.useState<Player[]>([]);
-  const [kaotika, setKaotika] = React.useState<Player[]>([]);
   
-  useEffect(() => {
-    setDravocar(players?.dravocar);
-    setKaotika(players?.kaotika);
-  }, [players]);
+  const { players} = useStore();
 
   return (
 
@@ -24,7 +16,7 @@ const Hud: React.FC = () => {
         <div className="flex flex-col items-center justify-start w-[46%] border-r-2 border-white p-2">
           <h2 className="text-4xl mb-2">KAOTIKA</h2>
           <div className="flex flex-wrap justify-center h-full overflow-y-auto">
-            {kaotika.map((player, index) => (
+            {players?.kaotika.map((player, index) => (
               <div
                 key={index}
                 className="m-1">
@@ -45,7 +37,7 @@ const Hud: React.FC = () => {
         <div className="flex flex-col items-center justify-start w-[46%] border-l-2 border-white p-2">
           <h2 className="text-4xl mb-2">DRAVOCAR</h2>
           <div className="flex flex-wrap justify-center h-full overflow-y-auto">
-            {dravocar.map((player, index) => (
+            {players?.dravocar.map((player, index) => (
               <div
                 key={index}
                 className="m-1">
