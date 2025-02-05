98772ccf639813e50ec3d10a7f34598ecd3ebbf5
ander <ander.zubizarreta@ikasle.aeg.eus>
2025-02-05 10:22:07 +0100

    [KBW-90][UPDATE]kaotika logo component

--- src/components/initScreen/KaotikaLengedsLogo.tsx
+++ src/components/initScreen/KaotikaLengedsLogo.tsx
@@ -3,10 +3,11 @@ import 'animate.css';
 
 const KaotikaLegendsLogo: React.FC = () => {
   return (
-    <div className="animate__animated animate__zoomInDown text-[6rem] absolute h-full w-full z-5 top-0">
-      LEGENDS OF K<span style={{ color: 'orange' }}>O</span>TIKA
+    <div
+      className="animate__animated animate__zoomInDown text-[6rem] h-full w-full flex justify-center items-center"
+      style={{ wordSpacing: '2rem' }}>
+      LEGENDS OF KA<span style={{ color: 'orange' }}>O</span>TIKA
     </div>
   );
 };
-
 export default KaotikaLegendsLogo;
