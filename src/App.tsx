99b5ac3bef9383d007bd6d58ce0893de22727041
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-02-05 09:21:22 +0100

    [KBW-89][ADD] the init screen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import BattleContainer from './components/battle/BattleContainer';
 import FinishTurn from './components/battle/finishTurn';
 import WaitingBattle from './components/battle/WaitingBattle';
 import Hud from './components/footer/Hud';
 import HeaderContainer from './components/header/HeaderContainer';
+import InitScreen from './components/initScreen/InitScreen';
 import { useSocketListeners } from './sockets/socketListeners';
 import battleImage from '/images/battle_bg.webp';
 
 function App() {
   const { startBattle, finishTurn } = useSocketListeners();
+  const [initScreen] = useState(true);
 
   return (
-    <div
-      className='w-screen h-screen bg-center bg-cover'
-      style={{ backgroundImage: `url(${battleImage})` }}>
+    <>
+      {initScreen
+        ? 
+        <InitScreen />
+        :
+        <div
+          className='w-screen h-screen bg-center bg-cover'
+          style={{ backgroundImage: `url(${battleImage})` }}>
 
-      {/* Header Container */}
-      {startBattle && <HeaderContainer />}
-      {/* Battle Container */}
-      {startBattle && <BattleContainer />}
-      {!startBattle && <WaitingBattle />}
+          {/* Header Container */}
+          {startBattle && <HeaderContainer />}
+          
+          {/* Battle Container */}
+          {startBattle && <BattleContainer />}
+          {!startBattle && <WaitingBattle />}
 
-      {finishTurn && startBattle && <FinishTurn />}
+          {finishTurn && startBattle && <FinishTurn />}
 
-      {/* Footer Container */}
-      <Hud />
-    </div>
-
-
+          {/* Footer Container */}
+          <Hud />
+        </div>
+      }
+    </>
   );
 }
 
