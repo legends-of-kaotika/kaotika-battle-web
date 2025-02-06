904fca3c3a0e55584368c051b1c7065a9d8045a4
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-06 09:46:58 +0100

    [KBW-97][ADD] Animation to disconnected player

--- src/components/battle/DisconnectUser.tsx
+++ src/components/battle/DisconnectUser.tsx
@@ -1,3 +1,4 @@
+import 'animate.css';
 import React from 'react';
 import useStore from '../../store/store';
 
@@ -5,7 +6,10 @@ const DisconnectUser: React.FC = () => {
   const { disconnectedPlayer } = useStore();
   return (
     disconnectedPlayer ? (
-      <div className='flex flex-col absolute bottom-[20%] h-[10%] w-[15%] justify-center bg-[#00000099]'>
+      <div
+        className='flex flex-col absolute bottom-[20%] h-[10%] w-[15%] justify-center bg-[#00000099] animate__animated animate__backOutUp animate__delay-3s'
+        style={{ animationDuration: '2s' }}
+      >
         <p className='text-2xl text-red-400'>{disconnectedPlayer}</p>
         <span className='text-2xl text-red-400'> has been disconnected!</span>
       </div>
