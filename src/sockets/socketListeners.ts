f5ca967633470776eac0e2208f2a8496b161042f
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-02-06 09:06:56 +0100

    [KBW-104][UPDATE] the game end socket on location

--- src/sockets/socketListeners.ts
+++ src/sockets/socketListeners.ts
@@ -16,13 +16,6 @@ export const useSocketListeners = () => {
   useEffect(() => {
     socket.emit('web-sendSocketId');
     socket.emit('web-sendUsers');
-    socket.on(socketName.GAME_END, (name : string) => {
-      console.log(name);
-    });
-
-    return (() => {
-      socket.off(socketName.GAME_END);
-    });
   }, []);
 
   useEffect(() => {
@@ -79,11 +72,9 @@ export const useSocketListeners = () => {
 
     function assignTurn(id: string) {
       console.log('assign turn to next person');
-
       setAttacker(getPlayerById(players, id)!);
       console.log('ATTACKER');
       console.log(attacker?.name);
-      
       setFinishTurn(false);
     }
 
@@ -92,6 +83,7 @@ export const useSocketListeners = () => {
       setPlayers(deletePlayerById(players, id));
     }
 
+    socket.on(socketName.GAME_END, () => {});
     socket.on(socketName.CONNECT, onConnect);
     socket.on(socketName.DISCONNECT, onDisconnect);
     socket.on(socketName.WEBSENDUSER, webSendUser);
@@ -115,7 +107,7 @@ export const useSocketListeners = () => {
       socket.off(socketName.UPDATEPLAYER, updatePlayer);
       socket.off(socketName.ASSIGNTURN, assignTurn);
       socket.off(socketName.REMOVEPLAYER, removePlayer);
-      
+      socket.off(socketName.GAME_END, );
     };
   }, [players]);
 
