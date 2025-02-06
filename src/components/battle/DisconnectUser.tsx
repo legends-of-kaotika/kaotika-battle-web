1c4e612379cdc3295b60aa598fd32d4fafb212d6
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-06 09:05:05 +0100

    [KBW-97][UPDATE] Disconnected player div display

--- src/components/battle/DisconnectUser.tsx
+++ src/components/battle/DisconnectUser.tsx
@@ -4,10 +4,12 @@ import useStore from '../../store/store';
 const DisconnectUser: React.FC = () => {
   const { disconnectedPlayer } = useStore();
   return (
-    <div className='flex flex-col absolute bottom-[20%] h-[10%] w-[15%] justify-center bg-[#00000099]'>
-      <p className='text-2xl text-red-400'>{disconnectedPlayer}</p>
-      <span className='text-2xl text-red-400'> has been disconnected!</span>
-    </div>
+    disconnectedPlayer ? (
+      <div className='flex flex-col absolute bottom-[20%] h-[10%] w-[15%] justify-center bg-[#00000099]'>
+        <p className='text-2xl text-red-400'>{disconnectedPlayer}</p>
+        <span className='text-2xl text-red-400'> has been disconnected!</span>
+      </div>
+    ) : null
   );
 };
 
