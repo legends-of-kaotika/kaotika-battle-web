85d880c4b15521ea4bc515c18fbc65810f3fc25c
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-06 10:18:35 +0100

    [KBW-97][ADD] Disconnected player timer to restart

--- src/components/battle/DisconnectUser.tsx
+++ src/components/battle/DisconnectUser.tsx
@@ -1,13 +1,28 @@
 import 'animate.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useStore from '../../store/store';
 
 const DisconnectUser: React.FC = () => {
   const { disconnectedPlayer } = useStore();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (disconnectedPlayer) {
+      setIsVisible(true);
+
+      const timer = setTimeout(() => {
+        setIsVisible(false);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [disconnectedPlayer]);
+
   return (
-    disconnectedPlayer ? (
+    isVisible ? (
       <div
-        className='flex flex-col absolute bottom-[20%] h-[10%] w-[15%] justify-center bg-[#00000099] animate__animated animate__backOutUp animate__delay-3s'
+        key={disconnectedPlayer}
+        className='flex flex-col absolute bottom-[50%] h-[10%] w-[15%] justify-center bg-[#00000099] animate__animated animate__backOutUp animate__delay-2s'
         style={{ animationDuration: '2s' }}
       >
         <p className='text-2xl text-red-400'>{disconnectedPlayer}</p>
